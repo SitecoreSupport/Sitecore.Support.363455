0467840a52e751406a00d5b7801ea066f0f17095
Liza1986 <kalinchukliza1986@gmail.com>
2019-10-07 13:22:35 +0300

    Fix the issue when selected values from the checkbox list are not listed in the Excel report.

---
+++ src/Sitecore.Support.363455/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.363455")]
+[assembly: AssemblyProduct("Sitecore.Support.363455")]
+[assembly: ComVisible(false)]
